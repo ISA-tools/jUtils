f555e9a7d9d66a15692910ae38f3747a749fdeff
Marco Brandizi <brandizi@ebi.ac.uk>
2013-11-11 12:39:30 +0000

    Adding ThreadedPipedWriter.

---
+++ src/main/java/uk/ac/ebi/utils/io/ThreadedPipedWriter.java
@@ -0,0 +1,227 @@
+package uk.ac.ebi.utils.io;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Writer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 
+ * TODO: Comment me!
+ * TODO: Never used/tested! 
+ * 
+ * <dl><dt>date</dt><dd>3 Nov 2013</dd></dl>
+ * @author Marco Brandizi
+ *
+ */
+public class ThreadedPipedWriter extends Writer
+{
+	private final PipedReader reader = new PipedReader ();
+	private final PipedWriter writer;
+	
+	private IOException ex = null;
+	
+	ExecutorService texec = Executors.newSingleThreadExecutor ();
+	
+	private abstract class IORunnable implements Runnable 
+	{
+		public final void run () 
+		{
+			try {
+				bareRun ();
+			}
+			catch ( IOException ex ) 
+			{
+				ThreadedPipedWriter.this.ex = new IOException ( 
+					"I/O problem while piping a writer to a reader: " + ex.getMessage (), ex );
+			}
+		}
+		public abstract void bareRun () throws IOException;
+	}
+	
+	
+	public ThreadedPipedWriter ( Writer base ) throws IOException
+	{
+		writer = new PipedWriter ( reader );
+	}
+	
+	@Override
+	public void write ( final char[] cbuf, final int off, final int len ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot write() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.write ( cbuf, off, len );
+			}
+		});
+	}
+
+	@Override
+	public void write ( final int c ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot write() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.write ( c );
+			}
+		});
+	}
+
+	@Override
+	public void write ( final char[] cbuf ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot write() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.write ( cbuf );
+			}
+		});
+	}
+
+	@Override
+	public void write ( final String str ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot write() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.write ( str );
+			}
+		});
+	}
+
+	@Override
+	public void write ( final String str, final int off, final int len ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot write() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.write ( str, off, len );
+			}
+		});
+	}
+
+	@Override
+	public Writer append ( final CharSequence csq ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot append() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.append ( csq );
+			}
+		});
+		return this;
+	}
+
+	@Override
+	public Writer append ( final CharSequence csq, final int start, final int end ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot append() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.append ( csq, start, end );
+			}
+		});
+		return this;
+	}
+
+	@Override
+	public Writer append ( final char c ) throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot append() due to previous exception:" + ex.getMessage (), ex );
+		
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.append ( c );
+			}
+		});
+		return this;
+	}
+
+	@Override
+	public void flush () throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot flush() due to previous exception:" + ex.getMessage (), ex );
+			
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.flush ();
+			}
+		});
+	}
+
+	@Override
+	public void close () throws IOException
+	{
+		if ( this.ex != null ) throw new IOException ( 
+			"Cannot close() due to previous exception:" + ex.getMessage (), ex );
+			
+		texec.submit ( new IORunnable() 
+		{
+			@Override
+			public void bareRun () throws IOException
+			{
+				writer.close ();
+			}
+		});
+	}
+	
+	public PipedReader getReader ()
+	{
+		return reader;
+	}
+
+	public IOException getIOException ()
+	{
+		return ex;
+	}
+	
+	public void resetException ()
+	{
+		this.ex = null;
+	}
+}
