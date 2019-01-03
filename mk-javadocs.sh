83ddd20b22040aa7d416822490098d70d140b3ee
Marco Brandizi <marco.brandizi@gmail.com>
2019-01-03 11:43:03 +0000

    Adding Javadocs creation.

---
+++ mk-javadocs.sh
@@ -0,0 +1,20 @@
+# Make the javadocs of each module under the main docs directory.
+
+echo -e "\n\n\t---- Making Javadocs for jutils\n"
+
+set -e 
+set -o pipefail
+
+export JAVADOC_REL_PATH=docs/javadocs
+cd "$(dirname $0)"
+
+for mod in jutils jutils-io jutils-j2ee
+do
+	echo -e "\n\n\tProcessing '$mod'\n"
+	rm -Rf "$JAVADOC_REL_PATH/$mod"
+	cd "$mod"
+	mvn javadoc:javadoc -DreportOutputDirectory="../$JAVADOC_REL_PATH"
+	cd ..
+done
+
+echo -e "\n\n\tThe End\n"
