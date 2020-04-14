8298bd59b115e52dedc67260ddfec6f90e2ccbca
Marco Brandizi <marco.brandizi@gmail.com>
2020-04-14 18:51:56 +0100

    Minor code quality improvement.

--- jutils/src/main/java/uk/ac/ebi/utils/threading/HackedBlockingQueue.java
+++ jutils/src/main/java/uk/ac/ebi/utils/threading/HackedBlockingQueue.java
@@ -82,10 +82,7 @@ public class HackedBlockingQueue<E> extends LinkedBlockingQueue<E>
 	public static ThreadPoolExecutor createExecutor ( int poolSize, int queueSize )
 	{
 		return new ThreadPoolExecutor (
-			poolSize, 
-			poolSize, 
-			0L, TimeUnit.MILLISECONDS, 
-			new HackedBlockingQueue<> ( queueSize ) 
+			poolSize, poolSize, 0L, TimeUnit.MILLISECONDS, new HackedBlockingQueue<> ( queueSize ) 
 		);		
 	}
 	
