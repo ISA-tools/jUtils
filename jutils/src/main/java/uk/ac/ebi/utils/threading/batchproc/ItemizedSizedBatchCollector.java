4f42676d16edb8c82ea0aaec251951df29d4e2a3
Marco Brandizi <marco.brandizi@gmail.com>
2020-04-16 12:28:24 +0100

    Merging some code cleaning from master.

--- jutils/src/main/java/uk/ac/ebi/utils/threading/batchproc/ItemizedSizedBatchCollector.java
+++ jutils/src/main/java/uk/ac/ebi/utils/threading/batchproc/ItemizedSizedBatchCollector.java
@@ -1,8 +1,8 @@
 package uk.ac.ebi.utils.threading.batchproc;
 
 /**
- * A common type of batch collector which manages collections of items and is considered full when it
- * has {@link #accumulator() accumulated} a {@link #maxBatchSize() given number of items}.
+ * A common type of batch collector which manages a flow of items as input and which considers the batch full when 
+ * it has {@link #accumulator() accumulated} a {@link #maxBatchSize() given number of items}.
  *
  * @author brandizi
  * <dl><dt>Date:</dt><dd>23 Nov 2019</dd></dl>
